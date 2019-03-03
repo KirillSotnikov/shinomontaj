637035a1969e97fabf246bc831d1c9a58f4aa487
unknown <sotnikov_k@outlook.com>
2019-03-04 00:13:39 +0200

    garanty page

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const pageId = $('main').attr('id');
+
+switch (pageId) {
+    case 'garantyPage': {
+        headerDropdown();
+        break;
+    }
+}
+
+
+function headerDropdown(){
+    $('.header_container_upper_box_arrow').on('click', function(){
+        $('.header_container_upper_box_dropdown').toggleClass('active');
+    });
+    $('.header_container_lowew_box_arrow').on('click', function(){
+        $('.header_container_lower_box .drowdown').toggleClass('active');
+    })
+}
